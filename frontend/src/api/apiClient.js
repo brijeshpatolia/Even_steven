ac571af7aefe460f0f4317d8d97d5a07f6a68e8f
brijeshpatolia <131127847+brijeshpatolia@users.noreply.github.com>
2025-06-19 19:52:18 +0530

    Update apiClient.js

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  //baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://even-steven-jhyx.vercel.app/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
